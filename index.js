5227fd2eec745ce7e7a73ae5f5d4fc51478e0e9e
Alaa Eddinne Benhmida <55157003+alaabenhmida@users.noreply.github.com>
2025-04-01 18:01:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -78,6 +78,11 @@ app.get('/api/server-info', async (req, res) => {
 });
 // Routes
 
+
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
 app.get('/health', (req, res) => {
   res.status(200).send('OK');
 });
